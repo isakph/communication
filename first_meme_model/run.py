36793b0b3c0385c1de58a377827802b29ba7b9ae
isakph@ <isakhaer@gmail.com>
2025-02-24 20:08:04 +0100

    removed some noise

--- first_meme_model/run.py
+++ first_meme_model/run.py
@@ -1,7 +1,5 @@
-import seaborn as sns
-
 from model import MemeModel
-# from server import server # I'll figure this stuff out later
+from server import server # I'll figure this stuff out later
 
 model = MemeModel(seed=2154651, meme_number=10)
 
@@ -10,9 +8,6 @@ for _ in range(20):
 
 data = model.datacollector.get_model_vars_dataframe()
 print(data)
-# for agent in model.agents:
-#     print(agent.meme)
-# g = sns.lineplot(data=data) # TODO: Visualisation
 
 # if __name__ == "__main__":
 #     server.port = 8521  # or any open port
